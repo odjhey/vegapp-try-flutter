135facd6213643fe33b4970ac6d2ac3ea7dc8116
Odee <odjhey@gmail.com>
2021-02-03 13:11:38 +0800

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -133,9 +133,32 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
       ),
       body: _buildSuggestions(),
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final tiles = _saved.map(
+        (WordPair pair) {
+          return ListTile(
+            title: Text(pair.asPascalCase, style: _biggerFont),
+          );
+        },
+      );
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved sugg'),
+          ),
+          body: ListView(
+            children: divided,
+          ));
+    }));
   }
 
   Widget _buildSuggestions() {
